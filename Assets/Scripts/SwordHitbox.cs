9220d5b5c674eb316e5f06587cdb25e9ee490513
soomnik <210624095+soomnik@users.noreply.github.com>
2025-06-06 02:21:43 +0900

    feat: enable collider

--- Assets/Scripts/SwordHitbox.cs
+++ Assets/Scripts/SwordHitbox.cs
@@ -1,10 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class SwordHitbox : MonoBehaviour
 {
     public int damage = 10;
+    private CapsuleCollider hitbox;
+
+    void Start()
+    {
+        hitbox = GetComponent<CapsuleCollider>();
+    }
+
+    public void EnableHitbox()
+    {
+        Debug.Log("Enabling hitbox");
+        hitbox.enabled = true;
+    }
+
+    public void DisableHitbox()
+    {
+        hitbox.enabled = false;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
