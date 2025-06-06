36f0b34e47e3540bbd2e2304970fad3e19e72e53
ansua152 <87211647+ansua152@users.noreply.github.com>
2025-06-06 18:27:08 +0900

    Defender collider enable

--- Assets/Agent_Defender/Script/AttackCollider.cs
+++ Assets/Agent_Defender/Script/AttackCollider.cs
@@ -4,20 +4,43 @@ using UnityEngine;
 
 public class AttackCollider : MonoBehaviour
 {
-    public GameObject A;
+    Animator parentanim;
+    private bool slashanimtriggered;
+    Collider hitbox;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        parentanim = GetComponentInParent<Animator>();
+        hitbox = GetComponent<Collider>();
+        slashanimtriggered = false;
+        hitbox.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (parentanim != null && !slashanimtriggered)
+        {
+            AnimatorStateInfo stateInfo = parentanim.GetCurrentAnimatorStateInfo(0);
+            if (stateInfo.IsName("Slash"))
+            {
+                slashanimtriggered = true;
+                StartCoroutine(EnableColliderTemporarily());
+            }
+        }
     }
 
+    IEnumerator EnableColliderTemporarily()
+    {
+        yield return new WaitForSeconds(0.5f);
+        hitbox.enabled = true;
+        yield return new WaitForSeconds(0.3f);
+        hitbox.enabled = false;
+        yield return new WaitForSeconds(1.0f);
+        slashanimtriggered = false;
+        
+    }
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log($"{name}, 이 안에 자유롭게 텍스트 입력 가능{other.gameObject.name}");
@@ -26,6 +49,6 @@ public class AttackCollider : MonoBehaviour
         {
             collisionobject.GetDamage(10);
         }
-        
+
     }
 }
